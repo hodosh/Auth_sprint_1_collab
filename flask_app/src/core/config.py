ba0e981e1305645e8961d1e1fa320c5e1e08850e
arthur hodosh <arthurhodosh2011@gmail.com>
2022-07-10 16:39:04 +0300

    separate models

---
+++ flask_app/src/core/config.py
@@ -0,0 +1,7 @@
+import os
+
+# Название проекта. Используется в Swagger-документации
+PROJECT_NAME = 'auth_service'
+
+# Корень проекта
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
