482b80c539e49a362c7a4d39ad53270ffbb56f16
Ilia Suvorkin <ilyafx@gmail.com>
2022-07-12 00:05:11 +0300

    Update API

--- api_mock/src/api/v1/user_role.py
+++ api_mock/src/api/v1/user_role.py
@@ -9,12 +9,12 @@ router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="refresh")
 
 @router.put(
-    '/user/assign_role',
+    '/{user_id}/role/{role_id}',
     responses={
         200: {'model': Role},
         401: {'model': UnauthorizedError, 'description': 'Error: Unauthorized'},
     },
     summary="🔓 🎩 Assign new role to user"
 )
-async def user_assign_role(id: EditRole, access_token = Depends(oauth2_scheme)) -> Role:
+async def user_assign_role(user_id: str, role_id: str,access_token = Depends(oauth2_scheme)) -> Role:
     pass
