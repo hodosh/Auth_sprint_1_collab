414466b54d7c88affb363ec0f4b3a1022ff6078c
arthur hodosh <arthurhodosh2011@gmail.com>
2022-07-17 10:37:23 +0300

    fix history decorator

--- flask_app/src/project/utils/history.py
+++ flask_app/src/project/utils/history.py
@@ -4,11 +4,12 @@ from project.models.models import UserHistory
 
 def log_activity(func):
     def wrapper(*args, **kwargs):
-        func(*args, **kwargs)
+        result = func(*args, **kwargs)
         user = token_auth.current_user()
         user_history = UserHistory(user_id=user.id, activity=func.__name__)
 
         database.session.add(user_history)
         database.session.commit()
+        return result
 
     return wrapper
