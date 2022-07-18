6dc92b290b42baef92b76eba9dc3ae8f12f3ca5a
arthur hodosh <arthurhodosh2011@gmail.com>
2022-07-18 21:55:24 +0300

    remove old & mock projects

--- flask_app_old/src/validators/validator.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional, Any
-from abc import ABC, abstractmethod
-
-
-class Validator(ABC):
-    @staticmethod
-    @abstractmethod
-    def validate(**kwargs) -> bool:
-        pass
--- flask_app_old/src/vew_models/message.py
+++
@@ -1,6 +0,0 @@
-from flask_app_old.src.models.base import ORJSONModel
-
-
-class Message(ORJSONModel):
-    head: str
-    body: str
